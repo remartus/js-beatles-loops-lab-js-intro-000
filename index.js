932e230482b7e1d9fc148918bb11d4bda8dbb239
Rose Martus <martus.rose@gmail.com>
2021-03-27 20:44:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var music = []
+  for(let i = 0; i < musicians.length; i++){
+    music[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return music
+}
+
+function johnLennonFacts(lennonFacts){
+  var excitedFacts = []
+  var i = 0
+  while(i < lennonFacts.length){
+    excitedFacts[i] = `${lennonFacts[i]}!!!`
+    i += 1
+  }
+  return excitedFacts
+}
+
+function iLoveTheBeatles(num){
+  var iLoveBeatles = []
+  var i = 0
+  do{
+    iLoveBeatles[i] = "I love the Beatles!"
+    num += 1
+    i += 1
+  }while(num < 15)
+  return iLoveBeatles
+}

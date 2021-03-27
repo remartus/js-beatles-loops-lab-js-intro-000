b9f2695a6b979f2be675d3c496c6736389e9b522
Rose Martus <martus.rose@gmail.com>
2021-03-27 20:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var music = []
+  for(let i = 0; i < musicians.length; i++){
+    music[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return music
+}
+function johnLennonFacts(lennonFacts){
+  var excitedFacts = []
+  var i = 0
+  while(i < lennonFacts.length){
+    excitedFacts[i] = `${lennonFacts[i]}!!!`
+  }
+  return excitedFacts
+}
